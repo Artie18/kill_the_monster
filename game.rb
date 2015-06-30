17fa1ba7fa7af1de220ae7932717a05ea6fdf3b4
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-07-01 01:10:01 +0300

    Added comments

--- game.rb
+++ game.rb
@@ -7,6 +7,9 @@ class Game
 
   attr_reader :status
 
+  # Initialize Game Class. And create all instance variables
+  #
+  # @return [Game] Game class
   def initialize
     @monster    = Monster.new
     @player     = Player.new
@@ -15,19 +18,32 @@ class Game
     @winner     = nil
   end
 
+  # Determens status of the finished game for the player
+  #
+  # @return [String] telling if player won or lost
   def status
     return '' if @winner.nil?
     @winner ? :win : :loose
   end
 
+  # Fetches current monster position and returns it
+  #
+  # @return [Integer] current monster position
   def monster_position
     @monster.position
   end
 
+  # Fetches current player position and returns it
+  #
+  # @return [Integer] current player position
   def player_position
     @player.position
   end
 
+  # Implement spaceship firing logic
+  #
+  # @param [Float] angle_degrees angle on which the ship has fired in degrees
+  # @return [Boolean] tells if operation was completed or not
   def fire(angle_degrees)
     return false unless @game_on
     angle_radians = angle_degrees * Math::PI / 180
@@ -39,6 +55,9 @@ class Game
     true
   end
 
+  # Passes the turn to the computer and implements it
+  #
+  # @return [Boolean] tells if operation was completed or not
   def next_turn
     return false unless @game_on
     @monster.move
@@ -49,20 +68,33 @@ class Game
     end
   end
 
+  # Tells if game is over or player can still play
+  #
+  # @return [Boolean] true - game is over, false - otherwise
   def over?
     !@game_on
   end
 
   private
 
+  # Tells if player was eaten by mosnter
+  #
+  # @return [Boolean] true - eaten, false - otherwise
   def eaten?
     monster_position == player_position
   end
 
+  # Tells if player killed the monster
+  #
+  # @param [Float] x_impact position along x where ball hits the ground
+  # @return [Boolean] true - monster was killed, false - otherwise
   def killed?(x_impact)
     x_impact.between?(*kill_zone)
   end
 
+  # Returns kill zone of a spaceship ball relatively monster
+  #
+  # @return [Array(Integer, Integer)] consists of beginning and the end of the kill zone
   def kill_zone
     [(monster_position - Spaceship::KILL_ZONE), (monster_position + Spaceship::KILL_ZONE)]
   end
--- monster.rb
+++ monster.rb
@@ -4,10 +4,17 @@ class Monster
 
   MOVMENT_ALTERATIONS = [-1, 1]
 
+  # Create a monster class
+  #
+  # @param [Range] position_variation is a possibility of monster positions
+  # @return [Monster] self class monster
   def initialize(position_variation = 1..100)
     @position = rand(position_variation)
   end
 
+  # Move the monster randomly
+  #
+  # @return [Integer] new position of the monster
   def move
     @position += MOVMENT_ALTERATIONS[rand(0..1)]
   end
